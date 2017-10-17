275046dd3ddb9c1974e682a746f597f7a134e159
DevasatSandeepKumar <devasat.sandeepkumar@gmail.com>
2017-10-17 15:37:30 +0530

    Android webview

---
+++ app/src/main/java/com/androidwebview/WebViewActivity.java
@@ -0,0 +1,26 @@
+package com.androidwebview;
+
+
+/**
+ * Created by Sandeep on 17/10/17.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends Activity {
+
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://princesandeep143.weebly.com/");
+
+    }
+
+}
